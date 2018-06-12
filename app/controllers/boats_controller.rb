a06bede3f66ef0c0a3d42c435fd586afe24c0bd5
Anthony Gawbill <gaw316@gmail.com>
2018-06-12 13:26:00 -0400

    create boat function

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,11 +3,30 @@ class BoatsController < ApplicationController
   end
 
   def new
+    @boat=Boat.new
   end
+
+  def create
+    boat=Boat.new(boat_params)
+    boat.user_id=current_user.id
+    if boat.save
+      redirect_to "/"
+    else
+      render "/boats/new"
+  end
+end
+
 
   def show
   end
 
   def edit
   end
+
+private
+
+  def boat_params
+    params.require(:boat).permit(:name, :max_load, :location, :user_id)
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "schedule#index"
-  devise_for :users
-  resources :boats 
+  resources :boats
   resources :jobs
   resources :schedule
-  
+
 end
