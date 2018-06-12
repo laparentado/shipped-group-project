a563790176b674d519029119da0d66dbefb4080a
Anthony Gawbill <gaw316@gmail.com>
2018-06-12 13:31:58 -0400

    added schedules controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'schedules/index'
+  get 'schedules/new'
+  get 'schedules/show'
+  get 'schedules/edit'
   devise_for :users
   root "schedule#index"
   resources :boats
