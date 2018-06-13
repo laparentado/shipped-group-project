11d47e918e52a2d1433380596a8702b0fe482119
Anthony Gawbill <gaw316@gmail.com>
2018-06-13 14:39:28 -0400

    validations work now

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,5 @@
 class Boat < ApplicationRecord
 has_many :users, :through => :jobs
   has_many :jobs
+  validates :name, :uniqueness => true
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :boat
   belongs_to :user
+  validates :description, :uniqueness => true
+  validates :cost, inclusion: { in: 0..100 }
 end
