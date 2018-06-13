5045349c0b37bb839b31b6d5cbab00507aac66b0
AMKlein2018 <adamklein0519@gmail.com>
2018-06-13 14:41:30 -0400

    updating show edit and new for paperclip images

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -6,4 +6,8 @@ has_many :users, :through => :jobs
 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+attr_accessor :remove_image
+  attr_accessible :remove_image
+
+
 end
