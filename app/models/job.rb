808d7511bb4f774d15f2b807845302b8205364c4
Anthony Gawbill <gaw316@gmail.com>
2018-06-13 15:06:05 -0400

    finished validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ApplicationRecord
   belongs_to :boat
   belongs_to :user
   validates :description, length: { maximum: 20 }
-  validates :cost, inclusion: { in: 0..100 }, message: "Must be less than $100"
+  validates :cost, numericality: { less_than_or_equal_to: 100,  only_integer: true }
 end
