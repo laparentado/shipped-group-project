016b35a5ddb03ac09f98075bca96405326c9bb51
Anthony Gawbill <gaw316@gmail.com>
2018-06-13 17:39:35 -0400

    fixed validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :boat
   belongs_to :user
-  validates :description, length: { maximum: 20 }
-  validates :cost, numericality: { less_than_or_equal_to: 100,  only_integer: true }
+  validates :description, length: { minimum: 20 }
+  validates :cost, numericality: { greater_than: 100,  only_integer: true }
 end
